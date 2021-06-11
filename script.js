22ead458ae28f4a8804e4540a66948568efe91ab
miss.daee <florchu161@gmail.com>
2021-06-11 09:57:15 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,50 @@
+//EJERCICIO 1
+// let text= prompt("ingrese texto");
+// console.log ("el texto ingresado contiene a: " + text.includes("a"));
+
+//EJERCICIO 2
+// let text= prompt("ingrese texto");
+
+// console.log ("texto en mayusculas: " + text.toUpperCase(text));
+// console.log ("texto en minuscula: " + text.toLowerCase(text));
+
+//EJERCICIO 3
+// let text= prompt("ingrese una palabra");
+
+// console.log ("la palabra " + text + " empieza con la letra " + text.charAt(0));
+
+//EJERCICIO 4
+// let text= prompt("ingrese una frase");
+
+// console.log ("su frase contiene " + text.length + " caracteres");
+
+//EJERCICIO 5
+// let text= prompt("ingrese un numero");
+
+// console.log ("su número contiene " + text.length + " caracteres");
+
+//EJERCICIO 6
+// let text= prompt("ingrese una palabra");
+
+// console.log ("la palabra " + text + " termina con la letra " + text[text.lenght-1]);
+
+//EJERCICIO 7
+// let text= prompt("ingrese su cadena de texto");
+
+// console.log (" su texto con caracteres reemplazados quedaría: " + text.replace(" ", "*"));
+
+//EJERCICIO 8
+// let letter= prompt("ingrese una letra");
+// let text= prompt("ingrese una cadena de texto");
+
+// console.log ("su texto contiene la letra " + letter + " : " + text.includes(letter));
+
+//EJERCICIO 9
+// let text= prompt("ingrese texto");
+
+// console.log (text.slice(0));
+
+// EJERCICIO 10
+// let text= prompt("ingrese su nombre en minusculas");
+
+// console.log ("hola " + text.toUpperCase(0));
